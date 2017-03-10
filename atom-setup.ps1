2a9d78b3096b7481fc6d2e1fcb0154cfc97e472d
Chris MacDonald <chris.macdonald@bigdataforhumans.com>
2017-03-10 14:49:48 +0000

    Initial

---
+++ atom-setup.ps1
@@ -0,0 +1,7 @@
+apm install atom-save-all
+apm install highlight-selected
+apm install linter
+apm install minimap
+apm install minimap-highlight-selected
+apm install tree-view-copy-relative-path
+apm install linter-eslint
